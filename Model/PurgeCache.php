ae1361134c6b2c2592418d5a26d2208bbdef0aa9
Glenn Slaven <glenn@section.io>
2016-09-25 17:43:37 +1000

    Capture cache purge events and send to section.io API for varnish banning

---
+++ Model/PurgeCache.php
@@ -0,0 +1,81 @@
+<?php
+namespace Sectionio\Metrics\Model;
+
+use Magento\Framework\Cache\InvalidateLogger;
+use Magento\Framework\App\DeploymentConfig;
+
+class PurgeCache
+{
+    const HEADER_X_MAGENTO_TAGS_PATTERN = 'X-Magento-Tags-Pattern';
+
+    /**
+     * @var \Magento\CacheInvalidate\Model\SocketFactory
+     */
+    protected $socketAdapterFactory;
+
+    /**
+     * @var InvalidateLogger
+     */
+    private $logger;
+
+    /** @var \Sectionio\Metrics\Helper\State $helper */
+    protected $state;
+
+    /** @var \Sectionio\Metrics\Helper\Aperture $aperture */
+    protected $aperture;
+
+    /** @var \Sectionio\Metrics\Helper\Data $helper */
+    protected $helper;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\CacheInvalidate\Model\SocketFactory $socketAdapterFactory
+     * @param InvalidateLogger $logger
+     * @param \Sectionio\Metrics\Helper\State $state
+     * @param \Sectionio\Metrics\Helper\Aperture $aperture
+     */
+    public function __construct(
+        \Magento\CacheInvalidate\Model\SocketFactory $socketAdapterFactory,
+        InvalidateLogger $logger,
+        \Sectionio\Metrics\Helper\State $state,
+        \Sectionio\Metrics\Helper\Aperture $aperture
+    ) {
+        $this->socketAdapterFactory = $socketAdapterFactory;
+        $this->logger = $logger;
+        $this->state = $state;
+        $this->aperture = $aperture;
+    }
+
+    /**
+     * Send curl purge request
+     * to invalidate cache by tags pattern
+     *
+     * @param string $tagsPattern
+     * @return bool Return true if successful; otherwise return false
+     */
+    public function sendPurgeRequest($tagsPattern)
+    {
+        $account_id = $this->state->getAccountId();
+        $application_id = $this->state->getApplicationId();
+        $environment_name = $this->state->getEnvironmentName();
+        $proxy_name = $this->state->getProxyName();
+
+        $uri = $this->aperture->generateUrl([
+            'api' => true,
+            'accountId' => $account_id,
+            'applicationId' => $application_id,
+            'environmentName' => $environment_name,
+            'proxyName' => $proxy_name,
+            'uriStem'   => '/state?banExpression=' . urlencode('obj.http.X-Magento-Tags ~ ' . $tagsPattern)
+        ]);
+
+        $info = $this->aperture->executeAuthRequest($uri, 'POST', []);
+        if ($info['http_code'] != 200) {
+            $this->logger->execute('Error executing purge: ' . $tagsPattern . ', Error Message: ' . $info['body_content']);
+            return false;
+        }
+        $this->logger->execute(compact('server', 'tagsPattern'));
+        return true;
+    }
+}
