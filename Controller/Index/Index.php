65654ada7e528360531c6d1f0b5ff2bbdc770909
Elijah Glover <elijah@elijah.com.au>
2016-09-25 16:02:52 +1000

    Fixed LetsEncrypt endpoint after refactoring

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -7,8 +7,8 @@ namespace Sectionio\Metrics\Controller\Index;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
-    // var \Sectionio\Metrics\Helper\Data $helper
-    protected $helper;
+    // var \Sectionio\Metrics\Helper\State $state
+    protected $state;
     // var \Sectionio\Metrics\Helper\Aperture $aperture
     protected $aperture;
     // var \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
@@ -17,11 +17,11 @@ class Index extends \Magento\Framework\App\Action\Action
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Sectionio\Metrics\Helper\Aperture $aperture,
-        \Sectionio\Metrics\Helper\Data $helper,
+        \Sectionio\Metrics\Helper\State $state,
         \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
     ) {
         $this->resultRawFactory = $resultRawFactory;
-        $this->helper = $helper;
+        $this->state = $state;
         $this->aperture = $aperture;
         parent::__construct($context);
     }
@@ -32,7 +32,7 @@ class Index extends \Magento\Framework\App\Action\Action
     public function execute()
     {
         $token = $this->getRequest()->getParam('token');
-        $response = $this->aperture->acmeChallenge($token, $this->status->getHostname());
+        $response = $this->aperture->acmeChallenge($token, $this->state->getHostname());
 
         $result = $this->resultRawFactory->create();
         $result = $result->setStatusHeader($response['http_code']);
