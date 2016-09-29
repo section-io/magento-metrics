7ffcb96f04ed8fade5906c863ee418ee65debe61
Jason Stangroome <jason@section.io>
2016-09-29 16:26:21 +1000

    Add editorconfig

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 pushd /workspace >>/dev/null
 
 rm --force "sectionio-magento-metrics-${EXTENSION_VERSION}.zip"
-zip -r "sectionio-magento-metrics-${EXTENSION_VERSION}.zip" . -x '.git/*' -x 'Dockerfile' -x 'build.sh'
+zip -r "sectionio-magento-metrics-${EXTENSION_VERSION}.zip" . -x '.git/*' -x 'Dockerfile' -x 'build.sh' -x '.editorconfig'
 
 popd >>/dev/null
 
