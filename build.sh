b0eeea998fe89bc31b91f70e34c63a5b43b50a61
Glenn Slaven <glenn@section.io>
2017-01-12 09:49:51 +1100

    Remove all . hidden files

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 pushd /workspace >>/dev/null
 
 rm --force "sectionio-magento-metrics-${EXTENSION_VERSION}.zip"
-zip -r "sectionio-magento-metrics-${EXTENSION_VERSION}.zip" . -x '.git/*' -x 'Dockerfile' -x 'build.sh' -x '.editorconfig' -x 'copy-identifiers.txt'
+zip -r "sectionio-magento-metrics-${EXTENSION_VERSION}.zip" . -x '.*' -x 'Dockerfile' -x 'build.sh' -x '.editorconfig' -x 'copy-identifiers.txt'
 
 popd >>/dev/null
 
