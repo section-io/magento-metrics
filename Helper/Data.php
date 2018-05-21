c491fc21d185b3eff9f3b50cff30f6ecd4bd50f8
Aaron Schreck <schreck.aaron@gmail.com>
2018-05-21 17:11:14 -0600

    Update function comments

--- Helper/Data.php
+++ Helper/Data.php
@@ -580,8 +580,15 @@ class Data extends AbstractHelper
             $applicationFactory->save();
         }
     }
-
-    /** get complete proxy image version for a given environment from aperture. */
+    
+    /**
+     * get complete proxy image version for a given environment from aperture.
+     *
+     * @param int $account_id
+     * @param int $application_id
+     *
+     * @return string
+     */
     public function getProxyVersion($account_id, $application_id)
     {
         /**generateApertureUrl takes an associative array */
@@ -605,8 +612,13 @@ class Data extends AbstractHelper
         /** returns only the version ie "5.2.1" */
         return explode(":", $image)[1];
     }
-
-    /** takes a full version like "5.2.1" and returns the major release (5) */
+    
+    /**
+     * Takes a full version like "5.2.1" and returns the major release (5)
+     * @param string $full_release
+     * 
+     * @return string
+     */
     public function getMajorRelease($full_release)
     {
         return explode(".", $full_release)[0];
